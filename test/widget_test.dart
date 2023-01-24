2f83e190c72f2e74ebfca6597f853ac6ce5ed671
arslanashraf006 <arslanashraf7864@gmail.com>
2023-01-24 18:34:15 +0500

    App: Added -> the in app review package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:in_app_purchase/main.dart';
+import 'package:inapppurchase/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
